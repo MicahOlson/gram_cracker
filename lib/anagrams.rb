0431f68aea00c8a3ec075562d2208a69ac82b7dc
Micah L. Olson <76067607+MicahOlson@users.noreply.github.com>
2021-02-26 10:34:52 -0800

    Refactor is-word check into its own method

--- lib/anagrams.rb
+++ lib/anagrams.rb
@@ -4,21 +4,19 @@ class Words
     @word_2 = word_2
   end
 
-  def compare
+  def is_word?(word)
     vowels = %w(a e i o u)
-    word_1_is_word = false
-    word_2_is_word = false
-    @word_1.each_char do |char|
+    is_word = false
+    word.each_char do |char|
       if vowels.include?(char)
-        word_1_is_word = true
+        is_word = true
       end
     end
-    @word_2.each_char do |char|
-      if vowels.include?(char)
-        word_2_is_word = true
-      end
-    end
-    if !word_1_is_word || !word_1_is_word
+    is_word
+  end
+
+  def compare
+    if !is_word?(@word_1) || !is_word?(@word_2)
       'Oops—please try again with actual words!'
     elsif @word_1.downcase.split('').sort == @word_2.downcase.split('').sort
       'These words are anagrams—they share all the same letters.'
