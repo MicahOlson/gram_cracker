76d5e038e53d258245505ea4babd71f8349442cc
Micah L. Olson <76067607+MicahOlson@users.noreply.github.com>
2021-02-26 15:20:21 -0800

    Add loop to allow user to continue or quit using app

--- gram_cracker.rb
+++ gram_cracker.rb
@@ -6,12 +6,18 @@ class Program
   def self.start
     system 'artii "Gram-Cracker!"'
     puts "-by Micah L. Olson\n\n"
-    puts "Enter two words or two phrases below to see if they are anagrams (sharing\nall the same letters), antigrams (sharing no common letters), or neither.\n\n"
-    print 'First word or phrase: '
-    str1 = gets.chomp
-    print 'Second word or phrase: '
-    str2 = gets.chomp
-    puts "\n=> #{GramCracker.new(str1, str2).crack}\n\n"
+    puts "Enter two words or two phrases below to see if they are anagrams (sharing\nall the same letters), antigrams (sharing no common letters), or neither.\n"
+    loop do
+      print "\nFirst word or phrase: "
+      str1 = gets.chomp
+      print 'Second word or phrase: '
+      str2 = gets.chomp
+      puts "\n=> #{GramCracker.new(str1, str2).crack}\n\n"
+      print 'Crack another gram?[y/n] '
+      again = gets.chomp
+      break if again == 'n'
+    end
+    puts "\nThank you for using Gram-Cracker! Goodbye."
   end
 end
 
