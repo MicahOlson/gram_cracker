7b9b86a108ac6cd66ebe8cbae0cc80c8d148fcaa
Micah L. Olson <76067607+MicahOlson@users.noreply.github.com>
2021-02-26 10:55:44 -0800

    Add logic to move antigram-check spec to green stage

--- lib/anagrams.rb
+++ lib/anagrams.rb
@@ -20,10 +20,18 @@ class Words
   end
 
   def compare
+    is_antigram = true
+    @word_1.each_char do |char|
+      if @word_2.include?(char)
+        is_antigram = false
+      end
+    end
     if !is_word?(@word_1) || !is_word?(@word_2)
       'Oops—please try again with actual words!'
     elsif is_anagram?
       'These words are anagrams—they share all the same letters.'
+    elsif is_antigram
+      'These words are antigrams—they share no common letters.'
     end
   end
 end
