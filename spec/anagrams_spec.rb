b47b491fdff9d9016ae9b9e8f3a4a129ffdd71f4
Micah L. Olson <76067607+MicahOlson@users.noreply.github.com>
2021-02-26 11:20:06 -0800

    Add red-stage spec to check if phrases are anagrams

--- spec/anagrams_spec.rb
+++ spec/anagrams_spec.rb
@@ -18,8 +18,13 @@ describe('Words#compare') do
     expect(words.compare).to(eq('Oops—please try again with actual words!'))
   end
 
-  it('checks if inputs are antigrams') do
+  it('checks if two words are antigrams') do
     words = Words.new('Friends', 'Boat')
     expect(words.compare).to(eq('These words are antigrams—they share no common letters.'))
+  end
+
+  it('checks if two phrases of multiple words are anagrams, ignoring spaces and punctuation.') do
+    words = Words.new('I think therefore I am.', "I fear to think I'm here!")
+    expect(words.compare).to(eq('These words are anagrams—they share all the same letters.'))
   end
 end
