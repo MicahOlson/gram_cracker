d65002af84e6661dfd052871abf11e8d47efe119
Micah L. Olson <76067607+MicahOlson@users.noreply.github.com>
2021-02-26 11:00:14 -0800

    Refactor antigram check into its own method

--- lib/anagrams.rb
+++ lib/anagrams.rb
@@ -19,18 +19,22 @@ class Words
     @word_1.downcase.split('').sort == @word_2.downcase.split('').sort
   end
 
-  def compare
+  def is_antigram?
     is_antigram = true
     @word_1.each_char do |char|
       if @word_2.include?(char)
         is_antigram = false
       end
+    is_antigram
     end
+  end
+
+  def compare
     if !is_word?(@word_1) || !is_word?(@word_2)
       'Oops—please try again with actual words!'
     elsif is_anagram?
       'These words are anagrams—they share all the same letters.'
-    elsif is_antigram
+    elsif is_antigram?
       'These words are antigrams—they share no common letters.'
     end
   end
