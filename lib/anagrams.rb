b8ba777f7c47c1e205c55452c0fb4ee9bafb2bf8
Micah L. Olson <76067607+MicahOlson@users.noreply.github.com>
2021-02-26 10:27:30 -0800

    Add logic to move is-word spec to green stage

--- lib/anagrams.rb
+++ lib/anagrams.rb
@@ -5,7 +5,22 @@ class Words
   end
 
   def compare
-    if @word_1.downcase.split('').sort == @word_2.downcase.split('').sort
+    vowels = %w(a e i o u)
+    word_1_is_word = false
+    word_2_is_word = false
+    @word_1.each_char do |char|
+      if vowels.include?(char)
+        word_1_is_word = true
+      end
+    end
+    @word_2.each_char do |char|
+      if vowels.include?(char)
+        word_2_is_word = true
+      end
+    end
+    if !word_1_is_word || !word_1_is_word
+      'Oops—please try again with actual words!'
+    elsif @word_1.downcase.split('').sort == @word_2.downcase.split('').sort
       'These words are anagrams—they share all the same letters.'
     end
   end
